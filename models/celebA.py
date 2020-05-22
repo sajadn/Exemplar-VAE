34f76c1b93d76ebaa227de1081b98d5547b40274
sajad <sajad.n@gmail.com>
2020-05-22 18:27:13 -0400

    add divide_size for celebA

--- models/celebA.py
+++ models/celebA.py
@@ -41,46 +41,47 @@ class VAE(AbsModel):
                 return x + self.f(x)
         self.train_data_size = train_data_size
 
+        d_size = args.divide_size
         self.q_z_layers = nn.Sequential(
-            nn.Conv2d(in_channels=self.args.input_size[0], out_channels=128, kernel_size=5, stride=2, padding=2),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(in_channels=self.args.input_size[0], out_channels=128//d_size, kernel_size=5, stride=2, padding=2),
+            nn.BatchNorm2d(128//d_size),
             nn.ELU(),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5, stride=2, padding=2),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(in_channels=128//divide_size, out_channels=256//d_size, kernel_size=5, stride=2, padding=2),
+            nn.BatchNorm2d(256//d_size),
             nn.ELU(),
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=5, stride=2, padding=2),
-            nn.BatchNorm2d(512),
+            nn.Conv2d(in_channels=256//d_size, out_channels=512//d_size, kernel_size=5, stride=2, padding=2),
+            nn.BatchNorm2d(512//d_size),
             nn.ELU(),
-            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=5, stride=2, padding=2),
-            nn.BatchNorm2d(1024),
+            nn.Conv2d(in_channels=512//d_size, out_channels=1024//d_size, kernel_size=5, stride=2, padding=2),
+            nn.BatchNorm2d(1024//d_size),
             nn.ELU(),
             Flatten()
         )
 
-        self.q_z_mean = nn.Sequential(nn.Linear(1024 * 4 * 4, self.args.z1_size),)
+        self.q_z_mean = nn.Sequential(nn.Linear(1024//d_size * 4 * 4, self.args.z1_size),)
 
-        self.q_z_logvar = nn.Sequential(nn.Linear(1024 * 4 * 4, self.args.z1_size), )
+        self.q_z_logvar = nn.Sequential(nn.Linear(1024//d_size * 4 * 4, self.args.z1_size), )
 
         self.p_x_layers = nn.Sequential(
-            nn.Linear(self.args.z1_size, 1024 * 4 * 4),
-            nn.BatchNorm1d(1024 * 4 * 4),
+            nn.Linear(self.args.z1_size, 1024//d_size * 4 * 4),
+            nn.BatchNorm1d(1024//d_size * 4 * 4),
             nn.ELU(),
-            UnFlatten(size=[1024, 4, 4]),
+            UnFlatten(size=[1024//d_size, 4, 4]),
             nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(512),
-            nn.ELU(),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(in_channels=1024//d_size, out_channels=512//d_size, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(512//d_size),
             nn.ELU(),
             nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(in_channels=512//d_size, out_channels=256//d_size, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(256//d_size),
+            nn.ELU(),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(in_channels=256//d_size, out_channels=128//d_size, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(128//d_size),
             nn.ELU(),
             nn.Upsample(scale_factor=2),
         )
 
         if self.args.input_type == 'gray' or self.args.input_type == 'continuous':
-            self.p_x_mean = nn.Sequential(nn.Conv2d(in_channels=128, out_channels=3, kernel_size=5, stride=1, padding=2), nn.Sigmoid())
+            self.p_x_mean = nn.Sequential(nn.Conv2d(in_channels=128//d_size, out_channels=3, kernel_size=5, stride=1, padding=2), nn.Sigmoid())
 
